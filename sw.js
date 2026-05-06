73a2684a2f21cec57fae072ab524a215d236974c
claupk <cegeve242@gmail.com>
2026-05-06 13:59:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'polyglot-v29';
+const CACHE_NAME = 'polyglot-v30';
 const assets = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
     self.skipWaiting();
